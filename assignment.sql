56d6d94403546085120897310ba1c9624baaa789
vignesh <VI351992@wipro.com>
2018-07-23 14:15:14 +0530

    assignment

---
+++ assignment.sql
@@ -0,0 +1,23 @@
+select co.contest_id,hacker_id,name, sum(ss.total_submission) Total_Submission,sum(ss.total_accepted_submission) Total_Accepted_Submission 
+into #temp1
+from contest co 
+inner join college cl on co.contest_id=cl.contest_id 
+inner join challenge ch on cl.college_id=ch.college_id 
+inner join submission_status ss on ch.challenge_id=ss.challenge_id
+group by co.contest_id,hacker_id,name
+
+select co.contest_id,hacker_id,name, sum(vs.total_views) Total_views,sum(vs.total_unique_views) Total_unique_views
+into #temp2
+ from contest co 
+inner join college cl on co.contest_id=cl.contest_id 
+inner join challenge ch on cl.college_id=ch.college_id 
+inner join view_status vs on ch.challenge_id=vs.challenge_id
+group by co.contest_id,hacker_id,name
+
+
+select isnull(A.contest_id,b.contest_id) CONTEST_ID,isnull(a.hacker_id,b.hacker_id) HACKER_ID,isnull(a.Name,b.name) NAME,isnull(a.Total_Submission,0) TOTAL_SUBMISSION,
+isnull(a.Total_Accepted_Submission,0) TOTAL_ACCEPTED_SUBMISSION,isnull(b.Total_views,0) TOTAL_VIEWS,isnull(b.Total_unique_views,0) TOTAL_UNIQUE_VIEWS 
+from #temp1 a
+full outer join  #temp2 b
+on A.contest_id = B.contest_id 
+where a.Total_Submission<>0 or a.Total_Accepted_Submission<>0 or b.Total_unique_views<>0 or b.Total_views<>0 order by 1 asc 
